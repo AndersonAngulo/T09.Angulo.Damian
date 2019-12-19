47c560151d50f86b41dfe5c22cc56aa2915f93e2
AndersonAngulo <56948485+andersonangulo@users.noreply.github.com>
2019-12-19 11:14:04 -0500

    Se hizo el llamado de la funcion 2 en app2 , en el directorio angulo

--- angulo/libreria1.py
+++ angulo/libreria1.py
@@ -5,3 +5,8 @@ def factorial_numero(numero):
         factorial=factorial*numero
         numero-=1
     return factorial
+#funcion2
+def saludar(nombre,animo):
+    print("Hola",nombre)
+    print("Te encuentras",animo,"\nEspero te sientas mucho mejor",nombre)
+
