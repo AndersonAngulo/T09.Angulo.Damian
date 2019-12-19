91984ad52d683fb7d925a9180c78671ea855c67b
AndersonAngulo <56948485+andersonangulo@users.noreply.github.com>
2019-12-19 11:16:25 -0500

    Se hizo el llamado de la funcion 4 en app4 , en el directorio angulo

--- angulo/libreria1.py
+++ angulo/libreria1.py
@@ -17,3 +17,18 @@ def numero_dni(numero):
         numero=input("ingrese numero de DNI valido:")
         numero_invalido=(len(numero)!=8 or numero.isdigit()==False)
     return (numero)
+#funcion4
+def promedio_notas(a,b,c):
+    a_invalido=(a<0 or a>20)
+    b_invalido = (a < 0 or a > 20)
+    c_invalido = (a < 0 or a > 20)
+    while(a_invalido==True):
+        a=int(input("ingrese nota 1(0-20):"))
+        a_invalido = (a < 0 or a > 20)
+    while (b_invalido == True):
+        b = int(input("ingrese nota 2(0-20):"))
+        b_invalido = (b < 0 or b > 20)
+    while (a_invalido == True):
+        c = int(input("ingrese nota 3(0-20):"))
+        c_invalido = (c < 0 or c > 20)
+    return ((a+b+c)/3)
