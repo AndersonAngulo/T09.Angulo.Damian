767e213ba80935af045a55abc2b2332154e86aca
AndersonAngulo <56948485+andersonangulo@users.noreply.github.com>
2019-12-19 11:15:23 -0500

    Se hizo el llamado de la funcion 3 en app3 , en el directorio angulo

--- angulo/libreria1.py
+++ angulo/libreria1.py
@@ -10,3 +10,10 @@ def saludar(nombre,animo):
     print("Hola",nombre)
     print("Te encuentras",animo,"\nEspero te sientas mucho mejor",nombre)
 
+#funcion3
+def numero_dni(numero):
+    numero_invalido = (len(numero) != 8 or numero.isdigit() == False)
+    while(numero_invalido==True):
+        numero=input("ingrese numero de DNI valido:")
+        numero_invalido=(len(numero)!=8 or numero.isdigit()==False)
+    return (numero)
